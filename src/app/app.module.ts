bd4d6c60aa6461b35490111b068b79bf9402c0aa
yoni <yamishav@cisco.com>
2017-02-21 17:36:44 +0200

    Merge branch 'master' of https://github.com/yonia1/ilan_tpc
    # Conflicts:
    #	README.md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { RouterModule } from "@angular/router";
+import { DEMO_APP_ROUTES } from "./routes";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(DEMO_APP_ROUTES),
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes.ts
@@ -0,0 +1,8 @@
+import {Routes} from '@angular/router';
+
+
+
+export const DEMO_APP_ROUTES: Routes = [
+
+
+];
