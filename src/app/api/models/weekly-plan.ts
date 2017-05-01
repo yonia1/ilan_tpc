45238f6639cdbf716bb9071369d5e76a8bb6dd3a
yoni <yamishav@cisco.com>
2017-05-01 17:44:51 +0300

    fix models after update from master

--- src/app/api/models/weekly-plan.ts
+++ src/app/api/models/weekly-plan.ts
@@ -2,5 +2,8 @@ import { WorkOut } from './workout';
 
 export class WeeklyPlan {
   weekly_total: number;
-  workouts: WorkOut[]
+  target_time: string;
+  race_distance: string;
+  race_date: string;
+  workouts: WorkOut[];
 }
